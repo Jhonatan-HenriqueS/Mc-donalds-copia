d06de1e1675235d1080e159c4e32651b0e208872
Jhonatan-HenriqueS <jhonatanhrcomercial@gmail.com>
2025-12-17 13:26:12 -0400

    feat: add_unique_the_restauyrant_slug

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
+    // Diz pro next que as imagens que tiver esse domínio serão validadas
+  },
 };
 
 export default nextConfig;
